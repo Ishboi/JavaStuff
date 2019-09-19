c90a1f8b599debede6886cc92e3a3125f724b223
Ishboi <bolachaseleite@gmail.com>
2019-09-19 20:52:38 +0100

    All tests made

--- DoneAutoBoxChallenge/src/com/fabiolearnsjava/Main.java
+++ DoneAutoBoxChallenge/src/com/fabiolearnsjava/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Bank bank = new Bank("National Austraia Bank");
 
+        if(bank.addBranch("Adelaide")) {
+            System.out.println("Adelaide branch created");
+        }
+
         bank.addBranch("Adelaide");
 
         bank.addCustomer("Adelaide", "Tim", 50.05);
@@ -19,6 +23,26 @@ public class Main {
         bank.addCustomerTransaction("Adelaide", "Mike", 1.65);
 
         bank.listCustomers("Adelaide", true);
+        bank.listCustomers("Sydney", true);
+
+        bank.addBranch("Melbourne");
+
+        if(!bank.addCustomer("Melbourne", "Brian", 5.33)) {
+            System.out.println("Error Melbourne branch does not exist");
+        }
+
+        if(!bank.addBranch("Adelaide")) {
+            System.out.println("Adelaide branch already exists");
+        }
+
+        if(!bank.addCustomerTransaction("Adelaide", "Fergus", 52.33)) {
+            System.out.println("Customer does not exist at branch");
+        }
+
+        if(!bank.addCustomer("Adelaide", "Tim", 12.21)) {
+            System.out.println("Customer Tim already exists");
+        }
+
 
 
     }
