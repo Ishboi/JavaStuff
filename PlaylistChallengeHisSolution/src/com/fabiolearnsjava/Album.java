ee0d6976bdce8bac7d3539b4d1c8de899fb0c1df
Ishboi <bolachaseleite@gmail.com>
2019-09-30 19:35:14 +0100

    Just implementing List interface for arraylists and linkedlists instead of using specific LinkedList types and ARrayList Types to demonstrate interface usage

--- PlaylistChallengeHisSolution/src/com/fabiolearnsjava/Album.java
+++ PlaylistChallengeHisSolution/src/com/fabiolearnsjava/Album.java
@@ -8,23 +8,20 @@ public class Album {
 
     private String name;
     private String artist;
-    private ArrayList<Song> songs;
+    //private ArrayList<Song> songs;
+    private SongList songList;
 
-    public Album(String name, String artist) {
-        this.name = name;
-        this.artist = artist;
-        this.songs = new ArrayList<Song>();
-    }
-
-    public boolean addSong(String title, double duration) {
-        if(findSong(title) == null) {
-            this.songs.add(new Song(title, duration));
-            return true;
+    class SongList {
+        private ArrayList<Song> songs = new ArrayList<>();
+        public boolean addSong(String title, double duration) {
+            if(findSong(title) == null) {
+                this.songs.add(new Song(title, duration));
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
 
-    private Song findSong(String title) {
+        private Song findSong(String title) {
         for(Song checkedSong: this.songs) {
             if(checkedSong.getTitle().equals(title)) {
                 return checkedSong;
@@ -33,10 +30,36 @@ public class Album {
         return null;
     }
 
+    }
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        //this.songs = new ArrayList<Song>();
+        this.songList = new SongList();
+    }
+
+    public boolean addSong(String title, double duration) {
+        if(songList.findSong(title) == null) {
+            this.songList.songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+//
+//    private Song findSong(String title) {
+//        for(Song checkedSong: this.songs) {
+//            if(checkedSong.getTitle().equals(title)) {
+//                return checkedSong;
+//            }
+//        }
+//        return null;
+//    }
+
     public boolean addToPlaylist(int trackNumber, List<Song> playlist) {
         int index = trackNumber -1;
-        if((index >= 0) && (index <= this.songs.size())) {
-            playlist.add(this.songs.get(index));
+        if((index >= 0) && (index <= this.songList.songs.size())) {
+            playlist.add(this.songList.songs.get(index));
             return true;
         }
         System.out.println("This album does not have a track " + trackNumber);
@@ -44,7 +67,7 @@ public class Album {
     }
 
     public boolean addToPlaylist(String title, List<Song> playlist) {
-        Song checkedSong = findSong(title);
+        Song checkedSong = songList.findSong(title);
         if(checkedSong != null) {
             playlist.add(checkedSong);
             return true;
