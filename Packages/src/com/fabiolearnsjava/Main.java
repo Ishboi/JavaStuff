0b1d72717c64841f13dcc2779c34d6688eb97a45
Ishboi <bolachaseleite@gmail.com>
2019-10-14 18:27:10 +0100

    BasicPackages continued

--- Packages/src/com/fabiolearnsjava/Main.java
+++ Packages/src/com/fabiolearnsjava/Main.java
@@ -3,6 +3,9 @@ package com.fabiolearnsjava;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        javafx.scene.Node node = null;
+        org.w3c.dom.Node anotherNode = null;
     }
 }
