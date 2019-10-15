dc771585609dc6e62ab7548eedb7b4494e98a540
Ishboi <bolachaseleite@gmail.com>
2019-10-15 19:21:45 +0100

    Scope introduction

--- Scope/src/com/fabiojava/series/test/Main.java
+++ Scope/src/com/fabiojava/series/test/Main.java
@@ -3,6 +3,16 @@ package com.fabiojava.series.test;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String varFour  = "this is private to main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+        System.out.println(varFour);
+
+        scopeInstance.timesTwo();
+        System.out.println("***********************************");
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        innerClass.timesTwo();
+
     }
 }
